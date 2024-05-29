6d530c5e3d10daefaf558316fb56d4f191c35828
mortal <mortal@123>
2024-05-29 13:16:43 +0530

    added add method with add class

--- DemoSolution/First/Program.cs
+++ DemoSolution/First/Program.cs
@@ -1,6 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using First;
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Edited");
+
+Console.WriteLine("Enter 1 st value");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter 2 nd value");
+int b = int.Parse(Console.ReadLine());
+
+Calculator c = new Calculator();
+c.Add(a,  b);
 
 Console.ReadLine();
